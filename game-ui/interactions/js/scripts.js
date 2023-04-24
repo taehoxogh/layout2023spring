4a92ec52b45d172244181427d270b144a12b733a
taehoxogh <98065809+taehoxogh@users.noreply.github.com>
2023-04-23 19:32:44 -0500

    Add files via upload

---
+++ game-ui/interactions/js/scripts.js
@@ -0,0 +1,97 @@
+
+
+
+document
+    .addEventListener('DOMContentLoaded', () => {
+        
+        
+        const stopwatch = document.getElementById('stopwatch')
+        stopwatch.addEventListener('click', (e) => {
+            stopwatch.classList.toggle('running')
+        })
+        
+
+        
+        document
+             .getElementById('hpbar1')
+             .addEventListener('click', () => {
+                 document
+                     .getElementById('hpbar1')
+                     .classList
+                     .toggle('middle')
+             })
+
+        document
+             .getElementById('hpbar2')
+             .addEventListener('click', () => {
+                 document
+                     .getElementById('hpbar2')
+                     .classList
+                     .toggle('low')
+             })
+     
+        document
+                  .getElementById('monsterButton')
+                  .addEventListener('click', () => {
+                     const actionBoxes = document.getElementsByClassName('bottomboxinner3')
+                     for (item of actionBoxes) {
+                         item.classList.remove('active')
+                     }
+                     document
+                          .getElementById('monsterBox')
+                          .classList
+                          .add('active')
+                  })
+
+        document
+                  .getElementById('potionButton')
+                  .addEventListener('click', () => {
+                     const actionBoxes = document.getElementsByClassName('bottomboxinner3')
+                     for (item of actionBoxes) {
+                         item.classList.remove('active')
+                     }
+                     document
+                          .getElementById('potionBox')
+                          .classList
+                          .add('active')
+                  })
+
+        document
+                  .getElementById('battleButton')
+                  .addEventListener('click', () => {
+                     const actionBoxes = document.getElementsByClassName('bottomboxinner3')
+                     for (item of actionBoxes) {
+                         item.classList.remove('active')
+                     }
+                     document
+                          .getElementById('battleBox')
+                          .classList
+                          .add('active')
+                  })
+
+        document
+                  .getElementById('GiveUpButton')
+                  .addEventListener('click', () => {
+                     const actionBoxes = document.getElementsByClassName('bottomboxinner3')
+                     for (item of actionBoxes) {
+                         item.classList.remove('active')
+                     }
+                     document
+                          .getElementById('GiveUpBox')
+                          .classList
+                          .add('active')
+                  })
+
+            })
+
+
+            const buttons = document.querySelectorAll('.menulist1');
+
+            for (const button of buttons) {
+              button.addEventListener('click', () => {
+                for (const button of buttons) {
+                  button.classList.remove('active');
+                }
+                button.classList.add('active');
+              });
+            }
